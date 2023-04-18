c1a23f1802f0041f1b9c788eeb4aa3d4bcb40e30
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-04-18 19:18:10 +0100

    Bump version v0.6.0 -> v0.6.1

--- repo_helper_devenv/__init__.py
+++ repo_helper_devenv/__init__.py
@@ -51,7 +51,7 @@ from virtualenv.run.session import Session  # type: ignore[import]
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.6.0"
+__version__: str = "0.6.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["mkdevenv", "read_pyvenv", "install_requirements"]
